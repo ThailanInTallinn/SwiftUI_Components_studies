b576cd8d16e5fc8ace450aac8a06819432409ad3
Thailan Gouvêa de Menezes <thailangm@icloud.com>
2026-09-16 21:53:50 +0300

    extends color class to include app bg custom color

---
+++ Sources/Components/Foundations/Color+Theme.swift
@@ -0,0 +1,11 @@
+//
+//  Color+Theme.swift
+//  Components
+//
+//  Created by Thailan Gouvea de Menezes on 16.09.2026.
+//
+import SwiftUI
+
+public extension Color {
+    static let AppBackground = Color("appBgColor", bundle: .module)
+}

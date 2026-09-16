38e1697da7b8e4bc9d421e302b2a8dfb82d4bce9
Thailan Gouvêa de Menezes <thailangm@icloud.com>
2026-09-16 16:23:57 +0300

    Adds package's basic structure

---
+++ Sources/Components/AppHeader.swift
@@ -0,0 +1,15 @@
+//
+//  AppHeader.swift
+//  Components
+//
+//  Created by Thailan Gouvea de Menezes on 16.09.2026.
+//
+
+import SwiftUI
+
+struct AppHeader: View {
+    
+    var body: some View {
+        
+    }
+}
